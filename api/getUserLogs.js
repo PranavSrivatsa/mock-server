93536574c1d0904d50aec7e2023ce3e7640cb273
PranavSrivatsa <pranav.srivatsa27@gmail.com>
2020-08-27 20:48:24 +0530

    Update getUserLogs.js
    port config for heroku

--- api/getUserLogs.js
+++ api/getUserLogs.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
 sampleJSON = require('../data/sampleJSON.json')
 
 app.get('/', (req, res) => {
   res.send(sampleJSON)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running...`)
 })

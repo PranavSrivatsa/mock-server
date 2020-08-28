9987f6f8978d18c6b9e8b0e0b20836b3b360538b
PranavSrivatsa <pranav.srivatsa27@gmail.com>
2020-08-28 17:47:38 +0530

    Update getUserLogs.js
    CORS test

--- api/getUserLogs.js
+++ api/getUserLogs.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 sampleJSON = require('../data/sampleJSON.json')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json(sampleJSON)
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log(`Server is running...`)
 })
